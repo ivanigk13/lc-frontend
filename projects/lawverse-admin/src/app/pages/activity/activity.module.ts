9f12a6ab990b96af57dc7987849827bbaf1c8bba
Iqbal Aditama <iqbaladitama91@gmail.com>
2022-03-30 11:48:55 +0700

    add activity pages and add navbar component

--- projects/lawverse-admin/src/app/pages/activity/activity.module.ts
+++ projects/lawverse-admin/src/app/pages/activity/activity.module.ts
@@ -2,8 +2,12 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { ButtonModule } from "primeng/button";
+import { CalendarModule } from "primeng/calendar";
 import { CheckboxModule } from "primeng/checkbox";
+import { DropdownModule } from "primeng/dropdown";
+import { FileUploadModule } from "primeng/fileupload";
 import { InputTextModule } from "primeng/inputtext";
+import { InputTextareaModule } from "primeng/inputtextarea";
 import { Menubar, MenubarModule } from "primeng/menubar";
 import { PasswordModule } from "primeng/password";
 import { TableModule } from "primeng/table";
@@ -18,10 +22,14 @@ import { ActivityRouter } from "./activity.router";
     imports: [
         FormsModule,
         CommonModule,
+        InputTextareaModule,
+        CalendarModule,
+        FileUploadModule,
         TableModule,
         ButtonModule,
         InputTextModule,
         ActivityRouter,
+        DropdownModule,
         MenubarModule,
         CheckboxModule,
         PasswordModule
