a8556df61234de2db8ef1ceafb53fd38e485d4ee
ivanigk13 <ivangrashinton@gmail.com>
2022-03-30 16:38:33 +0700

    adding profile dto

---
+++ projects/lawverse-admin/src/app/dto/profile/get-profile-dto-data-res.ts
@@ -0,0 +1,16 @@
+export class GetProfileDtoDataRes { 
+	 id!: string 
+	 userId!: string 
+	 industryId!: string 
+	 positionId!: string 
+	 cityId!: string 
+	 fileId!: string 
+	 socialMediaId!: string 
+	 fullName!: string 
+	 companyName!: string 
+	 phoneNumber!: string 
+	 postalCode!: string 
+	 version!: number 
+	 isActive!: boolean 
+} 
+
---
+++ projects/lawverse-admin/src/app/dto/profile/insert-profile-dto-req.ts
@@ -0,0 +1,13 @@
+export class InsertProfileDtoReq { 
+	 userId!: string 
+	 industryId!: string 
+	 positionId!: string 
+	 cityId?: string 
+	 fileId?: string 
+	 socialMediaId?: string 
+	 fullName!: string 
+	 companyName!: string 
+	 phoneNumber!: string 
+	 postalCode?: string 
+} 
+
