2d57024d944cd68af05572951a485a50406ba597
henatriana <henat29@gmail.com>
2022-04-04 10:00:15 +0700

    show industry list

--- projects/lawverse-admin/src/app/pages/industry/industry-list/industry-list.component.ts
+++ projects/lawverse-admin/src/app/pages/industry/industry-list/industry-list.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { GetIndustryDtoDataRes } from 'src/app/dto/industry/get-industry-dto-data-res';
+import { IndustryService } from 'src/app/service/industry.service';
 
 @Component({
   selector: 'app-industry-list',
   templateUrl: './industry-list.component.html',
   styleUrls: ['./industry-list.component.scss']
 })
-export class IndustryListComponent implements OnInit {
+export class IndustryListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  industries: GetIndustryDtoDataRes[] = []
+
+  industrySubs?: Subscription
+
+  constructor(private router: Router, private industryService: IndustryService) { }
 
   ngOnInit(): void {
+    this.getData()
   }
 
-  industries = [
-    { name: 'IT', code: 'IT' },
-    { name: 'HR', code: 'HR' }
-  ]
+  getData() {
+    this.industrySubs = this.industryService.getAll().subscribe(result => {
+      this.industries = result.data
+    })
+  }
+
+  onClick(): void {
+    this.router.navigateByUrl('/dashboard/industry/new')
+  }
+
+  ngOnDestroy(): void {
+    this.industrySubs.unsubscribe()
+  }
+
 }
