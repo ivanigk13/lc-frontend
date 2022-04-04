88459bd4cdcfc6b82cac8ec1be037cb78ab0b92e
henatriana <henat29@gmail.com>
2022-04-04 16:51:28 +0700

    fixed activity

--- projects/lawverse-admin/src/app/dto/activity/get-activity-dto-data-res.ts
+++ projects/lawverse-admin/src/app/dto/activity/get-activity-dto-data-res.ts
@@ -1,17 +1,17 @@
-export class GetActivityDtoDataRes { 
-	 id!: string 
-	 activityTypeId!: string 
-	 fileId!: string 
-	 paymentFileId!: string 
-	 activityName!: string 
-	 categoryId!: string 
-	 dateStart!: string 
-	 dateEnd!: string 
-	 timeStart!: string 
-	 timeEnd!: string 
-	 price!: number 
-	 location!: string 
-	 createdBy!: string 
-	 version!: number 
-} 
-
+export class GetActivityDtoDataRes {
+	id?: string
+	fileId?: string
+	categoryName?: string
+	activityTypeName?: string
+	transactionStatusId?: string
+	transactionStatusName?: string
+	paymentFileId?: string
+	activityName?: string
+	dateStart?: string
+	dateEnd?: string
+	timeStart?: string
+	timeEnd?: string
+	price?: number
+	location?: string
+	version?: number
+}
