99e7d6f949713f085e28d634a63f77e75992fd28
henatriana <henat29@gmail.com>
2022-04-04 09:57:04 +0700

    create transaction status

--- projects/lawverse-admin/src/app/dto/transaction-status/get-by-id-transaction-status-dto-res.ts
+++ projects/lawverse-admin/src/app/dto/transaction-status/get-by-id-transaction-status-dto-res.ts
@@ -1,5 +1,7 @@
-export class GetByIdTransactionStatusDtoRes { 
-	 msg?: string 
-	 data?: GetTransactionStatusDtoDataRes 
-} 
+import { GetTransactionStatusDtoDataRes } from "./get-transaction-status-dto-data-res"
+
+export class GetByIdTransactionStatusDtoRes {
+	msg?: string
+	data?: GetTransactionStatusDtoDataRes
+}
 
