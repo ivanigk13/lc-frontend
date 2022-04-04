741c1950680d08f5f0ebe27ef058b285d2569704
henatriana <henat29@gmail.com>
2022-04-04 10:01:35 +0700

    create role

--- projects/lawverse-admin/src/app/pages/role/role-list/role-list.component.ts
+++ projects/lawverse-admin/src/app/pages/role/role-list/role-list.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { GetRoleDtoDataRes } from 'src/app/dto/role/get-role-dto-data-res';
+import { RoleService } from 'src/app/service/role.service';
 
 @Component({
   selector: 'app-role-list',
   templateUrl: './role-list.component.html',
   styleUrls: ['./role-list.component.scss']
 })
-export class RoleListComponent implements OnInit {
+export class RoleListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  roles: GetRoleDtoDataRes[] = []
+
+  roleSubs?: Subscription
+
+  constructor(private router: Router, private roleService: RoleService) { }
 
   ngOnInit(): void {
+    this.getData()
   }
 
-  roles = [
-    {name:'Admin', code:'A01'},
-    {name:'Member', code:'M01'}
-  ]
+  getData(): void {
+    this.roleSubs = this.roleService.getAll().subscribe(result => {
+      this.roles = result.data
+    })
+  }
 
+  onClick(): void {
+    this.router.navigateByUrl('/dashboard/role/new')
+  }
+
+  ngOnDestroy(): void {
+    this.roleSubs?.unsubscribe()
+  }
 }
