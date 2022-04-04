0f63b9e8cc2a109d62af3fe1875ea28df78b27ab
ivanigk13 <ivangrashinton@gmail.com>
2022-04-04 15:03:32 +0700

    changing activity(html,ts), article(html), and thread (html, ts)

--- projects/lawverse-admin/src/app/pages/thread/thread-add/thread-add.component.ts
+++ projects/lawverse-admin/src/app/pages/thread/thread-add/thread-add.component.ts
@@ -1,19 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ThreadTypeList } from 'src/app/constant/thread-type-list';
+import { InsertPollingDetailDtoReq } from 'src/app/dto/polling-detail/insert-polling-detail-dto-req';
+import { InsertPollingHeaderDtoReq } from 'src/app/dto/polling-header/insert-polling-header-dto-req';
+import { GetThreadTypeDtoDataRes } from 'src/app/dto/thread-type/get-thread-type-dto-data-res';
+import { InsertThreadDtoReq } from 'src/app/dto/thread/insert-thread-dto-req';
+import { PollingDetailService } from 'src/app/service/polling-detail.service';
+import { PollingHeaderService } from 'src/app/service/polling-header.service';
+import { ThreadTypeService } from 'src/app/service/thread-type.service';
+import { ThreadService } from 'src/app/service/thread.service';
 
 @Component({
   selector: 'app-thread-add',
   templateUrl: './thread-add.component.html',
   styleUrls: ['./thread-add.component.scss']
 })
-export class ThreadAddComponent implements OnInit {
+export class ThreadAddComponent implements OnInit,OnDestroy {
 
   selectedDrop: any
   pollingDetailCounter : number = 2
+  pollingCode:string = ThreadTypeList.POLLING
   reduceDisable : boolean = true
 
-  constructor() { }
+  threadTypes : GetThreadTypeDtoDataRes[] = []
+  threadTypeSubs? : Subscription
+  threadTypeByIdSubs? : Subscription
+
+  insertThread : InsertThreadDtoReq = new InsertThreadDtoReq()
+  insertSubs? : Subscription
+
+  insertPollingHeader : InsertPollingHeaderDtoReq = new InsertPollingHeaderDtoReq()
+  insertPollingDetail : InsertPollingDetailDtoReq[] = []
+
+
+  constructor(private title:Title, private threadTypeService:ThreadTypeService, private threadService:ThreadService,
+              private pollingHeaderService:PollingHeaderService, private pollingDetailService:PollingDetailService,
+              private router:Router) {
+    title.setTitle('Insert Thread')
+  }
 
   ngOnInit(): void {
+    this.threadTypeSubs = this.threadTypeService.getAll().subscribe(result=>{
+      this.threadTypes = result.data
+    })
   }
 
   onAdd(): void {
@@ -28,17 +59,42 @@ export class ThreadAddComponent implements OnInit {
     } else {
       this.pollingDetailCounter -= 1      
     }
-
   }
 
-  createRange(num) {
+  createRange(num : number) {
     return new Array(num)
   }
 
-  threadTypes = [
-    { name: 'Reguler', id: 1 },
-    { name: 'Polling', id: 2 },
-    { name: 'Premium', id: 3 },
-  ]
+  insert(valid:boolean) {
+    if(valid){
+      this.insertSubs = this.threadService.insert(this.insertThread).subscribe(result=>{
+        if(result){
+          if(this.selectedDrop == this.pollingCode){
+            this.insertPollingHeader.threadId = result.data.id
+            this.pollingHeaderService.insert(this.insertPollingHeader).subscribe(result=>{
+              // this.insertPollingDetail.pollingHeaderId = result.data.id
+              // this.insertPollingDetail.pollingCounter = 0
+              // this.pollingDetailService.insert(this.insertPollingDetail).subscribe(result=>{
+              //   if(result) this.router.navigateByUrl('/thread')
+              // })
+            })
+          }
+        }
+      })
+    }
+  }
+
+  changeType() {
+    this.threadTypeByIdSubs = this.threadTypeService.getById(this.insertThread.threadTypeId).subscribe(result=>{
+      this.selectedDrop = result.data.threadTypeCode
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.threadTypeSubs?.unsubscribe()
+    this.threadTypeByIdSubs?.unsubscribe()
+    this.insertSubs?.unsubscribe()
+  }
+
 }
 
--- projects/lawverse-admin/src/app/pages/thread/thread-list/thread-list.component.ts
+++ projects/lawverse-admin/src/app/pages/thread/thread-list/thread-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-thread',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThreadListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private title:Title) {
+    title.setTitle('Thread List')
+  }
 
   ngOnInit(): void {
   }
