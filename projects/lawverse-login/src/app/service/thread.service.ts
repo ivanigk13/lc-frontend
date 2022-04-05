ec2b4d3547fdf8cde576ef9491c9d91c209ec535
henatriana <henat29@gmail.com>
2022-04-05 14:17:33 +0700

    service

---
+++ projects/lawverse-login/src/app/service/thread.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from "@angular/common/http"
+import { Injectable } from "@angular/core"
+import { Observable } from "rxjs"
+import { GetAllThreadDtoRes } from "../dto/thread/get-all-thread-dto-res"
+import { GetByIdThreadDtoRes } from "../dto/thread/get-by-id-thread-dto-res"
+import { InsertThreadDtoReq } from "../dto/thread/insert-thread-dto-req"
+import { InsertThreadDtoRes } from "../dto/thread/insert-thread-dto-res"
+import { UpdateThreadDtoReq } from "../dto/thread/update-thread-dto-req"
+import { UpdateThreadDtoRes } from "../dto/thread/update-thread-dto-res"
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ThreadService {
+    constructor(private http: HttpClient) { }
+
+    getAll(): Observable<GetAllThreadDtoRes> {
+        return this.http.get<GetAllThreadDtoRes>('http://localhost:8080/threads')
+    }
+
+    getById(id: string): Observable<GetByIdThreadDtoRes> {
+        return this.http.get<GetByIdThreadDtoRes>(`http://localhost:8080/threads/${id}`)
+    }
+
+    insert(insertThreadDtoReq: InsertThreadDtoReq, file?: File): Observable<InsertThreadDtoRes> {
+        const formData: FormData = new FormData()
+        formData.append('data', JSON.stringify(insertThreadDtoReq))
+        if (file) formData.append('file', file)
+        return this.http.post<InsertThreadDtoRes>('http://localhost:8080/threads', formData)
+    }
+
+    update(updateThreadDtoReq: UpdateThreadDtoReq): Observable<UpdateThreadDtoRes> {
+        return this.http.put<UpdateThreadDtoRes>(`http://localhost:8080/threads`, updateThreadDtoReq)
+    }
+
+}
