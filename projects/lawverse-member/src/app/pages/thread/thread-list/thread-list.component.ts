37315ec8a6d5bb574c84315151bc01ce58eb6b23
henatriana <henat29@gmail.com>
2022-04-05 14:15:13 +0700

    create and fixed thread polling

--- projects/lawverse-member/src/app/pages/thread/thread-list/thread-list.component.ts
+++ projects/lawverse-member/src/app/pages/thread/thread-list/thread-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-thread',
@@ -7,10 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThreadListComponent implements OnInit {
 
-
-  thread: string = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic ng, remaining essentially unchanged.It was popularised in the 1960s with the  release of Letraset sheets containing Lorem Ipsum passages, and more recently withdesktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-
-  constructor() { }
+  constructor(private title:Title) {
+    title.setTitle('Thread List')
+  }
 
   ngOnInit(): void {
   }
