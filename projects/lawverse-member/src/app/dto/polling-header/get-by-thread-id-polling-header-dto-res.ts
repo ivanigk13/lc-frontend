74c1bdd68388345ce15475ad6b6fc1929e661189
Iqbal Aditama <iqbaladitama91@gmail.com>
2022-04-07 21:49:26 +0700

    add getBythreadId in dto pollingHeader

---
+++ projects/lawverse-member/src/app/dto/polling-header/get-by-thread-id-polling-header-dto-res.ts
@@ -0,0 +1,6 @@
+import { GetPollingHeaderDtoDataRes } from "./get-polling-header-dto-data-res"
+
+export class GetByThreadIdPollingHeaderDtoRes{
+    msg?: string
+    data?: GetPollingHeaderDtoDataRes
+}
