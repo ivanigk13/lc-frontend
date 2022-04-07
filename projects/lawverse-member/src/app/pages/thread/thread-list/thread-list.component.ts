9cf31c1b893b1fff68a17cb46ad2fd1f54f767c4
ivanigk13 <ivangrashinton@gmail.com>
2022-04-07 11:09:07 +0700

    connecting thread to backend

--- projects/lawverse-member/src/app/pages/thread/thread-list/thread-list.component.ts
+++ projects/lawverse-member/src/app/pages/thread/thread-list/thread-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
+import { GetThreadDtoDataRes } from '../../../dto/thread/get-thread-dto-data-res';
+import { ThreadDetailService } from '../../../service/thread-detail.service';
+import { ThreadLikeService } from '../../../service/thread-like.service';
+import { ThreadService } from '../../../service/thread.service';
 
 @Component({
   selector: 'app-thread',
@@ -8,11 +13,31 @@ import { Title } from '@angular/platform-browser';
 })
 export class ThreadListComponent implements OnInit {
 
-  constructor(private title:Title) {
+  threads : GetThreadDtoDataRes[] = []
+  likeCounters : number[] = []
+  commentCounters : number[] = []
+
+  getAllThreadSubs? : Subscription
+  counterSubs? : Subscription
+
+  constructor(private title:Title, private threadService:ThreadService, private threadLikeService:ThreadLikeService,
+              private threadDetailService:ThreadDetailService) {
     title.setTitle('Thread List')
   }
 
   ngOnInit(): void {
+    this.getAllThreadSubs = this.threadService.getAll().subscribe(result=>{
+      this.threads = result.data
+      console.log(this.threads)
+      for(let i = 0; i < this.threads.length; i++){
+        this.threadLikeService.getLikeCounterByThreadId(this.threads[i].id).subscribe(result=>{
+          this.likeCounters.push(result)
+        })
+        this.threadDetailService.getCommentTotalByThreadId(this.threads[i].id).subscribe(result=>{
+          this.commentCounters.push(result)
+        })
+      }
+    })
   }
 
 }
