1e029cd601ca0d99dafe098122225affb4793e53
henatriana <henat29@gmail.com>
2022-04-07 09:11:29 +0700

    add activity data master

--- projects/lawverse-admin/src/app/pages/activity/activity.module.ts
+++ projects/lawverse-admin/src/app/pages/activity/activity.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from "@angular/core";
-import { ActivityAddComponent } from "./activity-add/activity-add.component";
 import { ActivityRouter } from "./activity.router";
 import { ActivityComponent } from './activity/activity.component';
-import { EventComponent } from './event/event.component';
-import { CourseComponent } from './course/course.component';
 import { BaseModule } from "src/app/base/base.module";
 import { ComponentModule } from "src/app/components/component.module";
 
 
 @NgModule({
     declarations: [
-        ActivityAddComponent,
         ActivityComponent,
-        EventComponent,
-        CourseComponent
     ],
     imports: [
         ComponentModule,
--- projects/lawverse-admin/src/app/pages/activity/activity.router.ts
+++ projects/lawverse-admin/src/app/pages/activity/activity.router.ts
@@ -1,9 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { ActivityAddComponent } from "./activity-add/activity-add.component";
 import { ActivityComponent } from "./activity/activity.component";
-import { CourseComponent } from "./course/course.component";
-import { EventComponent } from "./event/event.component";
 
 
 
@@ -11,18 +8,6 @@ const routes: Routes = [
     {
         path: '',
         component: ActivityComponent
-    },
-    {
-        path: 'new',
-        component: ActivityAddComponent
-    },
-    {
-        path: 'course',
-        component: CourseComponent
-    },
-    {
-        path: 'event',
-        component: EventComponent
     }
 ]
 
