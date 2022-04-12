4be3c501939d8dd99699f12a63ebdd864d5b7105
ivanigk13 <ivangrashinton@gmail.com>
2022-04-12 13:31:04 +0700

    adding social media service and dto for lawverse login

---
+++ projects/lawverse-login/src/app/dto/social-media/get-by-id-social-media-dto-res.ts
@@ -0,0 +1,7 @@
+import { GetSocialMediaDtoDataRes } from "./get-social-media-dto-data-res"
+
+export class GetByIdSocialMediaDtoRes { 
+	 msg?: string 
+	 data!: GetSocialMediaDtoDataRes
+} 
+
---
+++ projects/lawverse-login/src/app/dto/social-media/update-social-media-dto-res.ts
@@ -0,0 +1,7 @@
+import { UpdateSocialMediaDtoDataRes } from "./update-social-media-dto-data-res"
+
+export class UpdateSocialMediaDtoRes { 
+	 msg?: string 
+	 data!: UpdateSocialMediaDtoDataRes
+} 
+
