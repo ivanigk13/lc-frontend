f6e43eddb121be36afbe5cdd30b2524e4c7cb59c
ivanigk13 <ivangrashinton@gmail.com>
2022-04-12 13:36:20 +0700

    adjust variable in profile dto

--- projects/lawverse-login/src/app/dto/profile/get-profile-dto-data-res.ts
+++ projects/lawverse-login/src/app/dto/profile/get-profile-dto-data-res.ts
@@ -1,16 +1,17 @@
 export class GetProfileDtoDataRes { 
-	 id!: string 
-	 userId!: string 
-	 industryId!: string 
-	 positionId!: string 
-	 cityId!: string 
-	 fileId!: string 
-	 socialMediaId!: string 
-	 fullName!: string 
-	 companyName!: string 
-	 phoneNumber!: string 
-	 postalCode!: string 
-	 version!: number 
-	 isActive!: boolean 
+	id!: string
+	userId!: string
+	industryId!: string
+	positionId!: string
+	cityId?: string
+	provinceId?:string
+	fileId?: string 
+	socialMediaId?: string
+	fullName!: string
+	companyName!: string
+	phoneNumber!: string
+	postalCode?: string
+	version!: number
+	isActive!: boolean
 } 
 
