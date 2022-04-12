7c11b76512719f54c0fe1139f3444a2ff7335937
ivanigk13 <ivangrashinton@gmail.com>
2022-04-12 13:35:08 +0700

    fixing update profile html and ts code

--- projects/lawverse-member/src/app/pages/profile/profile.component.ts
+++ projects/lawverse-member/src/app/pages/profile/profile.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Subscription, concat } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { GetCityDtoDataRes } from '../../dto/city/get-city-dto-data-res';
 import { GetIndustryDtoDataRes } from '../../dto/industry/get-industry-dto-data-res';
 import { GetPositionDtoDataRes } from '../../dto/position/get-position-dto-data-res';
@@ -13,6 +13,9 @@ import { ProfileService } from '../../service/profile.service';
 import { ProvinceService } from '../../service/province.service';
 import { IndustryService } from '../../service/industry.service';
 import { PositionService } from '../../service/position.service';
+import { UpdateSocialMediaDtoReq } from '../../dto/social-media/update-social-media-dto-req';
+import { GetSocialMediaDtoDataRes } from '../../dto/social-media/get-social-media-dto-data-res';
+import { SocialMediaService } from '../../service/social-media.service';
 
 @Component({
   selector: 'app-profile',
@@ -22,8 +25,12 @@ import { PositionService } from '../../service/position.service';
 
 export class ProfileComponent implements OnInit,OnDestroy {
 
+  file? : File
   email! : string
   editProfile : boolean = true
+  positionName:string= ""
+  industryName:string=""
+  provinceId:string=""
   countries = []
   cities : GetCityDtoDataRes[] = []
   provinces : GetProvinceDtoDataRes[] = []
@@ -36,17 +43,24 @@ export class ProfileComponent implements OnInit,OnDestroy {
   getProvinceSubs? : Subscription
   getIndustrySubs? : Subscription
   getPositionSubs? : Subscription
+  getByIdCitySubs? : Subscription
   profile : GetProfileDtoDataRes = new GetProfileDtoDataRes()
   update : UpdateProfileDtoReq = new UpdateProfileDtoReq()
-  
+  updateSosmed : UpdateSocialMediaDtoReq = new UpdateSocialMediaDtoReq()
+  sosmed : GetSocialMediaDtoDataRes = new GetSocialMediaDtoDataRes()
 
   constructor(private title:Title, private loginService:LoginService, private profileService:ProfileService,
               private cityService:CityService, private provinceService:ProvinceService,
-              private industryService:IndustryService, private positionService:PositionService) {
+              private industryService:IndustryService, private positionService:PositionService,
+              private sosmedService : SocialMediaService) {
     title.setTitle('My Profile')
   }
 
   ngOnInit(): void {
+    this.getData()
+  }
+
+  getData() : void {
     this.email = this.loginService.getData().email
     this.getCitySubs = this.cityService.getAll().subscribe(result=>{
       this.cities = result.data
@@ -62,6 +76,27 @@ export class ProfileComponent implements OnInit,OnDestroy {
     })
     this.getByUserIdSubs = this.profileService.getByUserId(this.loginService.getData().id).subscribe(result=>{
       this.update = result.data
+      console.log(this.update)
+      if(this.update.positionId){
+        this.positionService.getById(this.update.positionId).subscribe(result=>{
+          this.positionName = result.data.positionName
+        })
+      }
+      if(this.update.industryId){
+        this.industryService.getById(this.update.industryId).subscribe(result=>{
+          this.industryName = result.data.industryName
+        })
+      }
+      if(this.update.cityId){
+        this.getByIdCitySubs = this.cityService.getById(this.update.cityId).subscribe(result=>{
+          this.provinceId = result.data.provinceId
+        })
+      }
+      if(this.update.socialMediaId){
+        this.sosmedService.getById(this.update.socialMediaId).subscribe(result=>{
+          this.updateSosmed = result.data
+        })
+      }
     })
   }
 
@@ -69,8 +104,20 @@ export class ProfileComponent implements OnInit,OnDestroy {
     this.editProfile = !this.editProfile
   }
 
+  setProvince() : void {
+    this.getByIdCitySubs = this.cityService.getById(this.update.cityId).subscribe(result=>{
+      this.provinceId = result.data.provinceId
+    })
+  }
+
   onUpdate(){
-    this.updateProfileSubs = this.profileService.update(this.update).subscribe()
+    this.updateProfileSubs = this.profileService.update(this.update, this.file).subscribe(result=>{
+      if(result){
+        this.sosmedService.update(this.updateSosmed).subscribe(result=>{
+          if(result) this.getData()
+        })
+      }
+    })
   }
   
   ngOnDestroy(): void {
@@ -80,6 +127,7 @@ export class ProfileComponent implements OnInit,OnDestroy {
     this.getProvinceSubs?.unsubscribe()
     this.getIndustrySubs?.unsubscribe()
     this.getPositionSubs?.unsubscribe()
+    this.getByIdCitySubs?.unsubscribe()
   }
 
 }
