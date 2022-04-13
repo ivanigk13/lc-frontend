8fbad5758bfea2c611cad9b593d5056bae2ea7de
Iqbal Aditama <iqbaladitama91@gmail.com>
2022-04-13 20:12:50 +0700

    add price to pay in add activity

--- projects/lawverse-member/src/app/pages/activity/activity-add/activity-add.component.ts
+++ projects/lawverse-member/src/app/pages/activity/activity-add/activity-add.component.ts
@@ -2,11 +2,11 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { first, firstValueFrom, map, Observable, Subscription } from 'rxjs';
-import { InsertActivityDtoReq } from 'src/app/dto/activity/insert-activity-dto-req';
-import { GetCategoryDtoDataRes } from 'src/app/dto/category/get-category-dto-data-res';
-import { ActivityTypeService } from 'src/app/service/activity-type.service';
-import { ActivityService } from 'src/app/service/activity.service';
-import { CategoryService } from 'src/app/service/category.service';
+import { InsertActivityDtoReq } from '../../../dto/activity/insert-activity-dto-req';
+import { GetCategoryDtoDataRes } from '../../../dto/category/get-category-dto-data-res';
+import { ActivityTypeService } from '../../../service/activity-type.service';
+import { ActivityService } from '../../../service/activity.service';
+import { CategoryService } from '../../../service/category.service';
 import { GetActivityTypeDtoDataRes } from '../../../dto/activity-type/get-activity-type-dto-data-res';
 
 @Component({
@@ -18,7 +18,8 @@ export class ActivityAddComponent implements OnInit {
 
   files: File[] = []
   categories$: Observable<GetCategoryDtoDataRes[]>
-  activityTypes$: Observable<GetActivityTypeDtoDataRes[]>
+  activityTypes: GetActivityTypeDtoDataRes[] = []
+  price : number
   insertActivityDtoReq: InsertActivityDtoReq = new InsertActivityDtoReq()
 
   constructor(private title: Title, private activityService: ActivityService, private router: Router,
@@ -30,18 +31,24 @@ export class ActivityAddComponent implements OnInit {
     this.getActivityType()
   }
 
+  async change(id : string) : Promise<void> {
+      this.price = await firstValueFrom(this.activityTypeService.getById(id).pipe(map(result => result.data.price)))
+  } 
+
   getCategory(): void {
     this.categories$ = this.categoryService.getAll().pipe(map(result => result.data))
   }
 
-  getActivityType(): void {
-    this.activityTypes$ = this.activityTypeService.getAll().pipe(map(result => result.data))
+
+
+  async getActivityType(): Promise<void> {
+    this.activityTypes = await firstValueFrom(this.activityTypeService.getAll().pipe(map(result => result.data)))
   }
 
   async insert(valid: boolean): Promise<void> {
     if (valid) {
       const result = await firstValueFrom(this.activityService.insert(this.insertActivityDtoReq, this.files).pipe(map(result => result.data)))
-      if (result) this.router.navigateByUrl('/member/event')
+      if (result) this.router.navigateByUrl('/member/activity/event')
     }
   }
 
