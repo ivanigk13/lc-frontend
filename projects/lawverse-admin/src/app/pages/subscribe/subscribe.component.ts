ce34f5df3fa22957618b0cabbf910715a5b41433
Iqbal Aditama <iqbaladitama91@gmail.com>
2022-04-13 20:14:22 +0700

    add additional function in on Approve function

--- projects/lawverse-admin/src/app/pages/subscribe/subscribe.component.ts
+++ projects/lawverse-admin/src/app/pages/subscribe/subscribe.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { firstValueFrom, map, Observable, Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { firstValueFrom, map, Observable  } from 'rxjs';
 import { UpdateOrderDtoReq } from '../../dto/order/update-order-dto-req';
 import { GetOrderDtoDataRes } from '../../dto/order/get-order-dto-data-res';
 import { OrderService } from '../../service/order.service';
+import { UserService } from '../../service/user.service';
+import { LoginService } from '../../service/login.service';
 
 @Component({
   selector: 'app-subscribe',
@@ -14,7 +16,8 @@ export class SubscribeComponent implements OnInit {
   subscribes$: Observable<GetOrderDtoDataRes[]>
   updateReq: UpdateOrderDtoReq = new UpdateOrderDtoReq()
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private userService : UserService,
+    private loginService : LoginService) { }
 
   ngOnInit(): void {
     this.getData()
@@ -24,10 +27,14 @@ export class SubscribeComponent implements OnInit {
     this.subscribes$ = this.orderService.getAllPendingSubscribe().pipe(map(result => result.data))
   }
 
-  async onApprove(id: string): Promise<void> {
+  async onApprove(id: string, userId : string, duration : number): Promise<void> {
     this.updateReq.id = id
     const result = await firstValueFrom(this.orderService.updateApprove(this.updateReq))
-    if(result) this.getData()
+    if(result) {
+      const user = this.loginService.getData()
+      const updateUser = await firstValueFrom(this.userService.updateSubcription(userId, duration, user.id))
+      if(updateUser) this.getData()
+    }
   }
 
   async onReject(id: string): Promise<void> {
