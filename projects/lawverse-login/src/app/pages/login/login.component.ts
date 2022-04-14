bdfc80b1c2c9012f57c56382fa7dace7bbf7ec22
ivanigk13 <ivangrashinton@gmail.com>
2022-04-14 15:35:22 +0700

    changing subscribe to async await in login

--- projects/lawverse-login/src/app/pages/login/login.component.ts
+++ projects/lawverse-login/src/app/pages/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ConfigService } from '../../service/app.config.service';
 import { AppConfig } from '../../api/appconfig';
-import { Subscription } from 'rxjs';
+import { firstValueFrom, Subscription } from 'rxjs';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { LoginService } from '../../service/login.service';
@@ -32,15 +32,8 @@ import { RoleList } from '../../constant/role-list';
 })
 export class LoginComponent implements OnInit, OnDestroy {
 
-  valCheck: string[] = ['remember'];
-
-  password: string;
-
   config: AppConfig;
-
   subscription: Subscription;
-
-  loginSubs?: Subscription
   roleCode: string
   loginDtoReq: LoginDtoReq = new LoginDtoReq()
 
@@ -56,23 +49,19 @@ export class LoginComponent implements OnInit, OnDestroy {
     });
   }
 
-  onLogin(valid: boolean) {
+  async onLogin(valid: boolean) {
     if (valid) {
-      this.loginSubs = this.loginService.login(this.loginDtoReq).subscribe(result => {
-        this.loginService.saveData(result)
-        this.roleCode = result.roleCode        
-        if(this.roleCode == RoleList.ADMIN) this.router.navigateByUrl('/admin/dashboard')
-        else this.router.navigateByUrl('/member/thread')
-      })
+      const resultLogin = await firstValueFrom(this.loginService.login(this.loginDtoReq))
+      this.loginService.saveData(resultLogin)
+      this.roleCode = resultLogin.roleCode        
+      if(this.roleCode == RoleList.ADMIN) this.router.navigateByUrl('/admin/dashboard')
+      else this.router.navigateByUrl('/member/thread')
     }
   }
 
   ngOnDestroy(): void {
     if (this.subscription) {
       this.subscription.unsubscribe();
-    }
-    if (this.loginSubs) {
-      this.loginSubs.unsubscribe()
     }
   }
 }
