12625065de3cc26e134586488a2b804e56332e46
henatriana <henat29@gmail.com>
2022-04-14 10:53:13 +0700

    create report page

---
+++ projects/lawverse-admin/src/app/dto/order-detail/get-income-dto-data-res.ts
@@ -0,0 +1,14 @@
+export class GetIncomeActivityDtoDataRes {
+    activityId?: string
+    createdAt?: string
+    invoice?: string
+    price?: number
+    activityName?: string
+    activityTypeName?: string
+    dateStart?: string
+    dateEnd?: string
+    timeStart?: string
+    timeEnd?: string
+    location?: string
+    total?: number
+}
--- projects/lawverse-admin/src/app/service/order-detail.service.ts
+++ projects/lawverse-admin/src/app/service/order-detail.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { GetAllOrderDetailDtoRes } from "../dto/order-detail/get-all-order-detail-dto-res";
 import { GetByIdOrderDetailDtoRes } from "../dto/order-detail/get-by-id-order-detail-dto-res";
+import { GetParticipantDtoRes } from "../dto/order-detail/get-participant-dto-res";
 import { InsertOrderDetailDtoReq } from "../dto/order-detail/insert-order-detail-dto-req";
 import { InsertOrderDetailDtoRes } from "../dto/order-detail/insert-order-detail-dto-res";
 
@@ -23,6 +24,14 @@ export class OrderDetailService {
 
     getById(id: string): Observable<GetByIdOrderDetailDtoRes> {
         return this.http.get<GetByIdOrderDetailDtoRes>(`http://localhost:8080/order-details/${id}`)
+    }
+
+    getParticipant(): Observable<GetParticipantDtoRes> {
+        return this.http.get<GetParticipantDtoRes>('http://localhost:8080/order-details/activity')
+    }
+
+    getIncome(): Observable<GetParticipantDtoRes> {
+        return this.http.get<GetParticipantDtoRes>('http://localhost:8080/order-details/activity-income')
     }
 
     insert(insertOrderDetailDtoReq: InsertOrderDetailDtoReq): Observable<InsertOrderDetailDtoRes> {
