444d0bc0b525e538154317fac0ce763d306263c4
Iqbal Aditama <iqbaladitama91@gmail.com>
2022-04-14 15:49:47 +0700

    delete subscribe

--- projects/lawverse-member/src/app/pages/thread/thread-single/thread-single.component.ts
+++ projects/lawverse-member/src/app/pages/thread/thread-single/thread-single.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { firstValueFrom, map} from 'rxjs';
+import { firstValueFrom, map } from 'rxjs';
 import { ThreadLikeService } from '../../../service/thread-like.service';
 import { GetThreadDetailDtoDataRes } from '../../../dto/thread-detail/get-thread-detail-dto-data-res';
 import { GetThreadDtoDataRes } from '../../../dto/thread/get-thread-dto-data-res';
@@ -49,14 +49,14 @@ export class ThreadSingleComponent implements OnInit {
   isBookmark: boolean = false
   isPremium: boolean = false
   userId: string
-  threadBookmarkId: string  
+  threadBookmarkId: string
 
   constructor(private activatedRoute: ActivatedRoute, private threadService: ThreadService,
     private threadDetailService: ThreadDetailService, private threadLikeService: ThreadLikeService,
     private loginService: LoginService, private pollingHeaderService: PollingHeaderService,
     private pollingDetailService: PollingDetailService, private pollingVoterService: PollingVoterService,
     private activityService: ActivityService, private threadBookmarkService: ThreadBookmarkService,
-    private router : Router) { }
+    private router: Router) { }
 
   ngOnInit(): void {
     this.getData()
@@ -81,7 +81,7 @@ export class ThreadSingleComponent implements OnInit {
         }
 
         const user = this.loginService.getData()
-        if(user.roleCode == RoleList.PREMIUM) this.isPremium = true
+        if (user.roleCode == RoleList.PREMIUM) this.isPremium = true
 
         this.userId = user.id
 
@@ -91,24 +91,24 @@ export class ThreadSingleComponent implements OnInit {
         }
 
         const bookmark = await firstValueFrom(this.threadBookmarkService.getThreadBookmarkByThreadId(this.thread.id))
-        if(bookmark.data.id) {
+        if (bookmark.data.id) {
           this.threadBookmarkId = bookmark.data.id
           this.isBookmark = true
         }
-      
+
         const like = await firstValueFrom(this.threadLikeService.getLikeCounterByThreadId(this.thread.id).pipe(map(result => result)))
-          if(like)this.likeCounter = like
+        if (like) this.likeCounter = like
 
         const totalComment = await firstValueFrom(this.threadDetailService.getCommentTotalByThreadId(this.thread.id).pipe(map(result => result)))
-          if(totalComment) this.commentCounter = totalComment        
+        if (totalComment) this.commentCounter = totalComment
       }
     }
   }
 
-  onOrder() : void {
+  onOrder(): void {
     this.router.navigateByUrl('/member/order/subscribtion')
   }
-  
+
 
   async getLastTwoEvent(): Promise<void> {
     this.events = await firstValueFrom(this.activityService.getLastTwoEvent().pipe(map(result => result.data)))
@@ -130,21 +130,21 @@ export class ThreadSingleComponent implements OnInit {
 
   async onVote(index: number, isVote: boolean): Promise<void> {
     const result = await firstValueFrom(this.pollingVoterService.getCountIdByHeaderId(this.pollingHeader.id, this.userId).pipe(map(result => result)))
-      if (result == 1) {
-        isVote = true
+    if (result == 1) {
+      isVote = true
+    }
+    if (isVote == false) {
+      this.pollingCounters = []
+      this.totalPolling = 0
+      this.pollingVoter.pollingDetailId = this.pollingDetails[index].id
+      const insert = await firstValueFrom(this.pollingVoterService.insert(this.pollingVoter))
+      if (insert) {
+        this.pollingDetails[index].pollingCounter = this.pollingDetails[index].pollingCounter + 1
+        const update = await firstValueFrom(this.pollingDetailService.update(this.pollingDetails[index]))
+        if (update) {
+          this.getData()
+        }
       }
-      if (isVote == false) {
-        this.pollingCounters = []
-        this.totalPolling = 0
-        this.pollingVoter.pollingDetailId = this.pollingDetails[index].id
-         const insert = await firstValueFrom(this.pollingVoterService.insert(this.pollingVoter))
-          if (insert) {
-            this.pollingDetails[index].pollingCounter = this.pollingDetails[index].pollingCounter + 1
-             const update = await firstValueFrom(this.pollingDetailService.update(this.pollingDetails[index]))
-              if (update) {
-                this.getData()
-              }            
-          }         
     }
   }
 
@@ -154,28 +154,27 @@ export class ThreadSingleComponent implements OnInit {
       this.threadLike.userId = this.loginService.getData().id
       this.threadLike.likeCounter = 1
       const insert = await firstValueFrom(this.threadLikeService.insert(this.threadLike))
-        if (insert) {
-          this.isLike = true
-          this.getData()
-        }
- 
+      if (insert) {
+        this.isLike = true
+        this.getData()
+      }
+
     }
   }
 
-  async onUnLike(like: boolean): Promise<void> {
+  async onDislike(id: string): Promise<void> {
 
   }
 
-  onBookmark(): void {
+  async onBookmark(): Promise<void> {
     if (!this.isBookmark) {
       this.threadBookmark.userId = this.userId
       this.threadBookmark.threadId = this.thread.id
-      this.threadBookmarkService.insert(this.threadBookmark).subscribe(result => {
-        if (result) {
-          this.isBookmark = true
-          this.getData()
-        }
-      })
+      const result = await firstValueFrom(this.threadBookmarkService.insert(this.threadBookmark))
+      if (result) {
+        this.isBookmark = true
+        this.getData()
+      }
     }
   }
 
@@ -189,9 +188,6 @@ export class ThreadSingleComponent implements OnInit {
     }
   }
 
-  async onDislike(id: string): Promise<void> {
-    this.threadLikeService.delete(id).subscribe()
-  }
 
   async onSubmit(data: boolean): Promise<void> {
     if (data) {
@@ -200,8 +196,8 @@ export class ThreadSingleComponent implements OnInit {
       this.threadDetail.threadId = this.thread.id
       const insert = await firstValueFrom(this.threadDetailService.insert(this.threadDetail))
       if (insert) {
-          this.getData()
-        }      
+        this.getData()
+      }
     }
   }
 }
