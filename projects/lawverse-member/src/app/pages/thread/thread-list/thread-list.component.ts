5f99afcc3a8ecccd167b5c5f92c32e3e80cea31c
Iqbal Aditama <iqbaladitama91@gmail.com>
2022-04-17 16:08:37 +0700

    change bookmarked thread to thread list

--- projects/lawverse-member/src/app/pages/thread/thread-list/thread-list.component.ts
+++ projects/lawverse-member/src/app/pages/thread/thread-list/thread-list.component.ts
@@ -8,6 +8,8 @@ import { GetThreadDtoDataRes } from '../../../dto/thread/get-thread-dto-data-res
 import { ThreadDetailService } from '../../../service/thread-detail.service';
 import { ThreadLikeService } from '../../../service/thread-like.service';
 import { ThreadService } from '../../../service/thread.service';
+import { ThreadBookmarkService } from '../../../service/thread-bookmark-service';
+import { LoginService } from '../../../service/login.service';
 
 @Component({
   selector: 'app-thread',
@@ -17,20 +19,25 @@ import { ThreadService } from '../../../service/thread.service';
 export class ThreadListComponent implements OnInit {
 
   threads: GetThreadDtoDataRes[] = []
+  bookmarkedThreads: GetThreadDtoDataRes[] = []
   likeCounters : number[] = []
-  commentCounters : number[] = []
+  commentCounters : number[] = []  
+  bookmarkedLikeCounters: number[] = []
+  bookmarkedCommentCounters: number[] = []
   events$: Observable<GetActivityDtoDataRes[]>
   courses$: Observable<GetActivityDtoDataRes[]>
   initialPage: number = 0
-  maxPage: number = 5
+  maxPage: number = 5  
 
   constructor(private title:Title, private router : Router, private threadService:ThreadService, private threadLikeService:ThreadLikeService,
-              private threadDetailService:ThreadDetailService, private activityService : ActivityService) {
+              private threadDetailService:ThreadDetailService, private activityService : ActivityService, 
+              private threadBookmarkService : ThreadBookmarkService, private loginService : LoginService) {
     this.title.setTitle('Thread List')
   }
 
   ngOnInit(): void {
     this.getAllThread()
+    this.getBookmarkedThread()
     this.getLastTwoCourse()
     this.getLastTwoEvent()
   }
@@ -43,10 +50,33 @@ export class ThreadListComponent implements OnInit {
          if(like) this.likeCounters.push(like)        
 
         let commentCounter = await firstValueFrom(this.threadDetailService.getCommentTotalByThreadId(this.threads[i].id).pipe(map(result => result)))
-        if(commentCounter) this.commentCounters.push(commentCounter)        
+        if(commentCounter) this.commentCounters.push(commentCounter)                   
       }
     }
   }
+
+  async getBookmarkedThread(): Promise<void> {
+    const userId = this.loginService.getData().id
+    const bookmarkList = await firstValueFrom(this.threadBookmarkService.getThreadBookmarkByUserId(userId).pipe(map(result => result.data)))
+    if (bookmarkList) {
+      for (let i = 0; i < bookmarkList.length; i++) {
+        const thread = await firstValueFrom(this.threadService.getById(bookmarkList[i].threadId).pipe(map(result => result.data)))
+        console.log(thread)
+        if (thread) {
+          this.bookmarkedThreads.push(thread)
+          
+          let like = await firstValueFrom(this.threadLikeService.getLikeCounterByThreadId(thread.id).pipe(map(result => result)))
+          console.log(like)
+          if (like) this.bookmarkedLikeCounters.push(like)
+
+          let totalComment = await firstValueFrom(this.threadDetailService.getCommentTotalByThreadId(thread.id).pipe(map(result => result)))
+          console.log(totalComment)
+          if (totalComment) this.bookmarkedCommentCounters.push(totalComment)
+        }
+      }
+    }
+  }
+
 
   async onScroll() : Promise<void> {
     this.initialPage = this.initialPage + 5
